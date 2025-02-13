43cd35d4fedef559637dbe90fc5897ace46c7ce7
afzalfaizi <afzaalm993@gmail.com>
2025-02-13 22:54:15 +0500

    add python-dotenv dependency and configure environment variable for GEMINI_API_KEY

--- src/agents_with_crewai/hello.py
+++ src/agents_with_crewai/hello.py
@@ -1,11 +1,10 @@
 from litellm import completion
 import os
 
-os.environ['GEMINI_API_KEY'] = "AIzaSyDhXD9SX9hjzvZai0rh_7QgXm0BDP0al9o"
 
 def call_gemini():
     response = completion(
         model="gemini/gemini-1.5-flash", 
-        messages=[{"role": "user", "content": "who is the founder of microsoft"}]
+        messages=[{"role": "user", "content": "who is the founder of Pakistan"}]
     )
     print(response['choices'][0]['message']['content'])
