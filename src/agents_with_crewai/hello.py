dae9e5972a20457484ef6df6fad3674b633abdc1
afzalfaizi <afzaalm993@gmail.com>
2025-02-13 22:35:26 +0500

    add dependency on litellm and update script to call_gemini function

--- src/agents_with_crewai/hello.py
+++ src/agents_with_crewai/hello.py
@@ -1,3 +1,11 @@
-def myfunction():
-    # Your code here
-    return "Hello, World!"
+from litellm import completion
+import os
+
+os.environ['GEMINI_API_KEY'] = "AIzaSyDhXD9SX9hjzvZai0rh_7QgXm0BDP0al9o"
+
+def call_gemini():
+    response = completion(
+        model="gemini/gemini-pro", 
+        messages=[{"role": "user", "content": "write code for saying hi from LiteLLM"}]
+    )
+    print(response['choices'][0]['message']['content'])
