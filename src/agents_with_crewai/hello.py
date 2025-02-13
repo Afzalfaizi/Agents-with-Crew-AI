96dc2c50de3c946cc6a8f5cdff33002ec6785497
afzalfaizi <afzaalm993@gmail.com>
2025-02-13 22:45:34 +0500

    update call_gemini function to use new model and change user message

--- src/agents_with_crewai/hello.py
+++ src/agents_with_crewai/hello.py
@@ -5,7 +5,7 @@ os.environ['GEMINI_API_KEY'] = "AIzaSyDhXD9SX9hjzvZai0rh_7QgXm0BDP0al9o"
 
 def call_gemini():
     response = completion(
-        model="gemini/gemini-pro", 
-        messages=[{"role": "user", "content": "write code for saying hi from LiteLLM"}]
+        model="gemini/gemini-1.5-flash", 
+        messages=[{"role": "user", "content": "who is the founder of microsoft"}]
     )
     print(response['choices'][0]['message']['content'])
