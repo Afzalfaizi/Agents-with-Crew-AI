99de7ea14ce40cf4253739be1f14e30eb6c6f38c
afzalfaizi <afzaalm993@gmail.com>
2025-02-16 21:12:48 +0500

    update user message in call_gemini function to ask about founder of crewai framework

--- src/agents_with_crewai/hello.py
+++ src/agents_with_crewai/hello.py
@@ -5,6 +5,6 @@ import os
 def call_gemini():
     response = completion(
         model="gemini/gemini-1.5-flash", 
-        messages=[{"role": "user", "content": "who is the founder of OpenAI"}]
+        messages=[{"role": "user", "content": "who is the founder of crewai framework?"}]
     )
     print(response['choices'][0]['message']['content'])
